688b6dbfee4c81d5a2f7ef950a8b9a6cdb5dba29
LoiselK <lnkleijnen@gmail.com>
2025-10-10 01:58:13 +0000

    Updating Visuals for Click Container

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+import carrot from "./Carrot.png";
 import "./style.css";
 
 let counter: number = 0;
 
 document.body.innerHTML = `
-  <img id="increment" src="${exampleIconUrl}" class="icon" />
-  <h2>Total Coins: <span id="counter"> ${counter} </span></h2>
+  <div class="main">
+    <img id="increment" src="${carrot}" class="icon" />
+    <h2>Total Coins: <span id="counter"> ${counter} </span></h2>
+  </div>
 `;
 
 const clickImage = document.getElementById("increment")!;
